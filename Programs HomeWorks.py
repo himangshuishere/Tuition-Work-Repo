f60d1a321f6f23a0c615f0ac9e45ccc6776d53af
Himangshu De <dehimangshu2020@gmail.com>
2021-04-16 11:33:05 +0530

    Modified Program HomeWorks.py file with certain color changes

--- Programs HomeWorks.py
+++ Programs HomeWorks.py
@@ -1,3 +1,13 @@
+GREEN = '\u001b[32m'
+CYAN = '\u001b[36m'
+RESET = '\u001b[0m'
+RED = '\u001b[31m'
+MAGENTA = '\u001b[35m'
+
+import colorama
+colorama.init()
+print(f"{GREEN}")
+
 L = [17,19, 21, 27, 31]
 # Right shift of first term to last term
 for i in range(len(L)):
@@ -20,7 +30,8 @@ for i in range(len(L)):
 
 print("List after shifting the last element to the first: ", L)
 
-
+print("---------------------------------------------------------------------------")
+print(f"{CYAN}")
 # !-------------------------------------------------------!
 # !                    Functions                          !
 # !-------------------------------------------------------!
@@ -30,10 +41,8 @@ print("List after shifting the last element to the first: ", L)
 def circle(radius: float = ..., pieValue: float = 3.14)-> list:
     """
     This function returns the `list` containing `perimeter` and `area` respectively.
-
     Args:
     `radius`: This argument can be `float` or `int` data type
-
     `pieValue`: This argument should always be in `float` data type
     """
     
@@ -46,9 +55,12 @@ def circle(radius: float = ..., pieValue: float = 3.14)-> list:
 radiusCircle = float(input("Enter radius in cm: "))
 pieCircle = float(input("Enter pie's value: "))
 circleAreaPerimeter = circle(radiusCircle, pieCircle)
-print("Perimeter of circle is: ", circleAreaPerimeter[0]," cm")
-print("Area of Circle is: ", circleAreaPerimeter[1], " cm²")
+print(f"Perimeter of circle is: {circleAreaPerimeter[0]:.2f}cm")
+print(f"Area of Circle is: {circleAreaPerimeter[1]}cm²")
     
+print()
+print("-----------------------------------------------------------")
+print(f"{RED}")
 
 def grade(schoolName: str=..., studentName : str = ..., SubjectMarks: list = [], totalSubjects: int = 6, totalMarksForEachSubject: int = 80)-> None:
     print("+-------------------Report Card----------------------+")
@@ -82,10 +94,16 @@ def grade(schoolName: str=..., studentName : str = ..., SubjectMarks: list = [],
         print()
         print("Student has failed and demoted to current class")
     
+schoolName = input("Enter school name: ")
+studentName = input("Enter student name: ")
+subjectMarks = eval(input("Enter subjects marks in list format: "))
+totalSubjectsNumber = int(input("Enter total number of subjects: "))
+totalMarksforEachSubject = int(input("Enter total marks alloted for each subject: "))
+grade(schoolName,studentName, subjectMarks, totalSubjectsNumber, totalMarksforEachSubject)
 
-# grade()
-
-
+print()
+print("----------------------------------------------")
+print(f"{GREEN}")
 def factorial(number: int = ...)-> int:
     fact = 1
     for i in range(1, number+1):
